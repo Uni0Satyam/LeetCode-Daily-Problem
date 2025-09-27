564936a812d9674f175a2ff8aada70417c808b26
Satyam Kushwaha <165474613+Uni0Satyam@users.noreply.github.com>
2025-09-27 19:08:58 +0530

    Date - 27/09/2025

---
+++ Leetcode812.java
@@ -0,0 +1,25 @@
+public class Leetcode812 {
+    public static double largestTriangleArea(int[][] points) {
+        double maxArea = 0.0;
+        int n = points.length;
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i+1; j < n-1; j++) {
+                for (int k = j+1; k < n; k++) {
+                    int x1 = points[i][0];
+                    int y1 = points[i][1];
+                    int x2 = points[j][0]; 
+                    int y2 = points[j][1];
+                    int x3 = points[k][0]; 
+                    int y3 = points[k][1];
+                    double area = 0.5 * Math.abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+                    maxArea = Math.max(maxArea,area);
+                }
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int points[][] = {{0,0},{0,1},{1,0},{0,2},{2,0}};
+        System.out.println(largestTriangleArea(points));
+    }
+}
