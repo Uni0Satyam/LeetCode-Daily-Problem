cb5d2f1635cef79f91309e0d4e75814ad7a2052c
Satyam Kushwaha <165474613+Uni0Satyam@users.noreply.github.com>
2025-09-28 23:48:38 +0530

    28/09/2025

---
+++ Leetcode976.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Leetcode976 {
+    public static int largestPerimeter(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i = n-1; i >= 2; i--){
+            if (nums[i-2]+nums[i-1]>nums[i]) {
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return 0;
+    }
+}
